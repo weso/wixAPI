dc7acb5e1ea798ab990091550147da25eda0ace5
castrofernandez <castrofernandez@gmail.com>
2014-10-09 09:41:12 +0200

    Visualisation methods

--- api/api.py
+++ api/api.py
@@ -216,6 +216,20 @@ def list_observations_years_array():
     return JSONEncoder(request, years)
 
 ##########################################################################################
+##                                    VISUALISATIONS                                    ##
+##########################################################################################
+
+@app.route("/visualisations/<indicator_code>/<area_code>/<year>")
+def list_visualisations(indicator_code, area_code, year):
+    visualisations = ObservationRepository(url_root=request.url_root).find_visualisations(indicator_code, area_code, year)
+    return JSONEncoder(request, visualisations)
+
+@app.route("/visualisations/<indicator_code>/<area_code>/<year>")
+def list_visualisations_maxBars(indicator_code, area_code, year, maxBars):
+    visualisations = ObservationRepository(url_root=request.url_root).find_visualisations(indicator_code, area_code, year, maxBars)
+    return JSONEncoder(request, visualisations)
+
+##########################################################################################
 ##                                        MAIN                                          ##
 ##########################################################################################
 
